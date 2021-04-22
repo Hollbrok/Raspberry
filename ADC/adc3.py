086b01bb6bb446ed7f05cc2a683d70a07302227e
Daniil Talashkevich <talashkevich.da@phystech.edu>
2021-04-22 11:31:22 +0300

    Add 3rd and 4-th scripts for ADC

--- ADC/adc3.py
+++ ADC/adc3.py
@@ -1,65 +1,68 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode (GPIO.BCM)
+GPIO.setmode(GPIO.BCM)
 
 DAC_PIN_LIST      = [26, 19, 13,  6,  5, 11,  9, 10]
-POTENTIOMETR_PIN  =  17
-CMP_OUT_PIN       =   4
+POTENTIOMETR_PIN  = 17
+CMP_OUT_PIN       = 4
 
-GPIO.setup (POTENTIOMETR_PIN, GPIO.OUT)
-GPIO.setup (DAC_PIN_LIST, GPIO.OUT)
+GPIO.setup(POTENTIOMETR_PIN, GPIO.OUT)
+GPIO.setup(DAC_PIN_LIST, GPIO.OUT)
+GPIO.setup(CMP_OUT_PIN, GPIO.IN)
 
-GPIO.setup (CMP_OUT_PIN, GPIO.IN)
 
-def decToBinList( decNumber ) :
-    vector = [0, 0, 0, 0, 0, 0, 0, 0]
+def decToBinList(decNumber) :
+    result = [0, 0, 0, 0, 0, 0, 0, 0]
 
     for i in range (0, 8) :
         if (decNumber % 2) == 1 :
-            vector [i] = 1
+            result [i] = 1+-
         decNumber = decNumber // 2
 
         if decNumber == 0 :
-            return vector
+            return result
     
-    return vector
+    return result
 
-def num2dac( number ) :
-    vector = decToBinList (number)
-    GPIO.output (DAC_PIN_LIST, 0)
+
+def num2dac(number) :
+    result = decToBinList(number)
+    GPIO.output(DAC_PIN_LIST, 0)
 
     for i in range (0, 8) :
-        if vector[i] == 1 :
-            GPIO.output (DAC_PIN_LIST [7 - i], 1)
+        if result[i] == 1 :
+            GPIO.output(DAC_PIN_LIST[7 - i], 1)
+
 
 def search() :
-    voltage = 0
+    volt = 0
 
     for i in range (0, 255) :
-        num2dac (i)
-        time.sleep (0.0005)
-        if GPIO.input (CMP_OUT_PIN) != 1 :
-            voltage = 3.3 * float (i) / 255
+        num2dac(i)
+        time.sleep(0.0005)
+        if GPIO.input(CMP_OUT_PIN) != 1 :
+            volt = 3.3 * float (i) / 255
             break
     
-    return voltage
+    return volt
+
 
 def binSearch() :
     value = 0
-    step  = 128
+    i     = 128
 
     while True :
-        num2dac (value)
-        time.sleep (0.0005)
+        num2dac(value)
+        time.sleep(0.0005)
 
-        if GPIO.input (CMP_OUT_PIN) == 1 :
-            value += step
+        if GPIO.input(CMP_OUT_PIN) == 1 :
+            value += i
         else :
-            value -= step
+            value -= i
 
-        step = int (step / 2)
-        if step == 0 :
+        i = int(i / 2)
+        if i == 0 :
             break
 
     if value < 0 :
@@ -68,19 +71,20 @@ def binSearch() :
 
 
 
-GPIO.output (POTENTIOMETR_PIN, 1)
+GPIO.output(POTENTIOMETR_PIN, 1)
 
-voltage = 0
-newVoltage = 0
+lastVolt = 0
+NewVolt = 0
 
-while True:
-    
-    newVoltage = binSearch()
+while True:    
+    NewVolt = binSearch()
 
-    if abs (newVoltage - voltage) > 1.0 / 255.0 :
-        print (newVoltage, int (newVoltage * 255 / 3.3))
-        voltage = newVoltage
+    if abs(NewVolt - lastVolt) > 1.0 / 255.0 :
+        print(NewVolt, int (NewVolt * 255 / 3.3))
+        lastVolt = NewVolt
 
-GPIO.output (DAC_PIN_LIST, 0)
-GPIO.output (POTENTIOMETR_PIN)
-GPIO.cleanup ()
+
+
+GPIO.output(DAC_PIN_LIST, 0)
+GPIO.output(POTENTIOMETR_PIN)
+GPIO.cleanup()
--- ADC/adc4.py
+++ ADC/adc4.py
@@ -1,95 +1,98 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode (GPIO.BCM)
+GPIO.setmode(GPIO.BCM)
 
 DAC_PIN_LIST      = [26, 19, 13,  6,  5, 11,  9, 10]
-DIODS_PIN_LIST    = [21, 20, 16, 12,  7,  8, 25, 24]
-POTENTIOMETR_PIN  =  17
-CMP_OUT_PIN       =   4
+POTENTIOMETR_PIN  = 17
+CMP_OUT_PIN       = 4
 
-GPIO.setup (POTENTIOMETR_PIN, GPIO.OUT)
-GPIO.setup (DAC_PIN_LIST, GPIO.OUT)
-GPIO.setup (DIODS_PIN_LIST, GPIO.OUT)
+GPIO.setup(POTENTIOMETR_PIN, GPIO.OUT)
+GPIO.setup(DAC_PIN_LIST, GPIO.OUT)
+GPIO.setup(CMP_OUT_PIN, GPIO.IN)
 
-GPIO.setup (CMP_OUT_PIN, GPIO.IN)
+DIODS_PIN_LIST = [21, 20, 16, 12,  7,  8, 25, 24]
+GPIO.setup(DIODS_PIN_LIST, GPIO.OUT)
 
-def decToBinList( decNumber ) :
-    vector = [0, 0, 0, 0, 0, 0, 0, 0]
+
+
+def decToBinList(decNumber) :
+    result = [0, 0, 0, 0, 0, 0, 0, 0]
 
     for i in range (0, 8) :
         if (decNumber % 2) == 1 :
-            vector [i] = 1
+            result [i] = 1+-
         decNumber = decNumber // 2
 
         if decNumber == 0 :
-            return vector
+            return result
     
-    return vector
+    return result
 
-def num2dac( number ) :
-    vector = decToBinList (number)
+def num2dac(number) :
+    result = decToBinList(number)
     GPIO.output (DAC_PIN_LIST, 0)
 
     for i in range (0, 8) :
-        if vector[i] == 1 :
-            GPIO.output (DAC_PIN_LIST [7 - i], 1)
+        if result[i] == 1 :
+            GPIO.output(DAC_PIN_LIST [7 - i], 1)
 
-def num2dio( number ) :
-    vector = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+def num2VolLvl(number) :
+    vec = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     if (number >= 252):
-        vector = [1, 1, 1, 1, 1, 1, 1, 1]
+        vec = [1, 1, 1, 1, 1, 1, 1, 1]
     elif (number > 256 / 8 * 7):
-        vector = [0, 1, 1, 1, 1, 1, 1, 1]
+        vec = [0, 1, 1, 1, 1, 1, 1, 1]
     elif (number > 256 / 8 * 6):
-        vector  = [0, 0, 1, 1, 1, 1, 1, 1]
+        vec  = [0, 0, 1, 1, 1, 1, 1, 1]
     elif (number > 256 / 8 * 5):
-        vector  = [0, 0, 0, 1, 1, 1, 1, 1]
+        vec  = [0, 0, 0, 1, 1, 1, 1, 1]
     elif (number > 256 / 8 * 4):
-        vector  = [0, 0, 0, 0, 1, 1, 1, 1]
+        vec  = [0, 0, 0, 0, 1, 1, 1, 1]
     elif (number > 256 / 8 * 3):
-        vector  = [0, 0, 0, 0, 0, 1, 1, 1]
+        vec  = [0, 0, 0, 0, 0, 1, 1, 1]
     elif (number > 256 / 8 * 2):
-        vector  = [0, 0, 0, 0, 0, 0, 1, 1]
+        vec  = [0, 0, 0, 0, 0, 0, 1, 1]
     elif (number > 256 / 8 * 1):
-        vector  = [0, 0, 0, 0, 0, 0, 0, 1]
+        vec  = [0, 0, 0, 0, 0, 0, 0, 1]
     else:
-        vector  = [0, 0, 0, 0, 0, 0, 0, 0]
+        vec  = [0, 0, 0, 0, 0, 0, 0, 0]
 
-    GPIO.output (DIODS_PIN_LIST, 0)
+    GPIO.output(DIODS_PIN_LIST, 0)
 
     for i in range (0, 8) :
-        if vector[i] == 1 :
-            GPIO.output (DIODS_PIN_LIST [7 - i], 1)
+        if vec[i] == 1 :
+            GPIO.output(DIODS_PIN_LIST [7 - i], 1)
 
 def search() :
     voltage = 0
 
     for i in range (0, 255) :
-        num2dac (i)
-        time.sleep (0.0005)
-        if GPIO.input (CMP_OUT_PIN) != 1 :
-            voltage = 3.3 * float (i) / 255
+        num2dac(i)
+        time.sleep(0.001)
+        if GPIO.input(CMP_OUT_PIN) != 1 :
+            voltage = 3.3 * float(i) / 255
             break
     
     return voltage
 
+
 def binSearch() :
     value = 0
-    step  = 128
+    i  = 128
 
     while True :
-        num2dac (value)
-        time.sleep (0.0005)
+        num2dac(value)
+        time.sleep(0.0005)
 
-        if GPIO.input (CMP_OUT_PIN) == 1 :
-            value += step
+        if GPIO.input(CMP_OUT_PIN) == 1 :
+            value += i
         else :
-            value -= step
+            value -= i
 
-        step = int (step / 2)
-        if step == 0 :
+        i = int(i / 2)
+        if i == 0 :
             break
 
     if value < 0 :
@@ -98,16 +101,18 @@ def binSearch() :
 
 
 
-GPIO.output (POTENTIOMETR_PIN, 1)
+GPIO.output(POTENTIOMETR_PIN, 1)
 
 value = 0
 
 while True:
     
-    value = binSearch ()
-    num2dio (value)
-    print (value)
+    value = binSearch()
+    num2VolLvl(value)
+    print(value)
 
-GPIO.output (DAC_PIN_LIST, 0)
-GPIO.output (POTENTIOMETR_PIN)
-GPIO.cleanup ()
+
+
+GPIO.output(DAC_PIN_LIST, 0)
+GPIO.output(POTENTIOMETR_PIN)
+GPIO.cleanup()
